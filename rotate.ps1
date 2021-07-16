503110a0027f646e3ad1f31bba6d3583f50f2f2f
Jacob Maltais <felixnya@protonmail.com>
2021-07-16 15:26:40 -0400

    Updated the script by adding logs (it became the very thing it swore to destroy)

--- rotate.ps1
+++ rotate.ps1
@@ -1,28 +1,32 @@
 $path = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
 
-$log_list = Import-Csv $path/config.csv -Delimiter "~" -Header 'log_path', 'toKeep', 'log_name'
+$log_list = Import-Csv $path/config.csv -Delimiter ";" -Header 'log_path', 'toKeep', 'log_name'
 
+"-----STARTED $(get-date)------" | Out-File -FilePath $path\logs\Debug.log -Append
+"" | Out-File -FilePath $path\logs\Debug.log -Append
 foreach($log in $log_list){
-    
-	#Check if the rotate folder exist. If not, it will create one for you.
-	
-	if(!(Test-Path "$($log.log_path)\rotate")){
-		md "$($log.log_path)\rotate"
-	}
-	
+    "~~ Starting operation on $($log.log_path)\$($log.log_name)" | Out-File -FilePath $path\logs\Debug.log -Append
     #Copy the log and erase the main one. If it's empty, it skips it.
+    if(!(Test-Path "$($log.log_path)\rotate")){
+		"Directory does not exist.. Creating it." | Out-File -FilePath $path\logs\Debug.log -Append
+        md "$($log.log_path)\rotate"
+    }
 
     if(Get-Content "$($log.log_path)\$($log.log_name)"){
+		"Moving and clearing content..." | Out-File -FilePath $path\logs\Debug.log -Append
         cp "$($log.log_path)\$($log.log_name)" "$($log.log_path)\rotate\$(Get-Date -Format "MM-dd-yyyy")--$($log.log_name)"
 		Clear-Content "$($log.log_path)\$($log.log_name)"
     }
+	else{
+		"Log is empty, ignoring.." | Out-File -FilePath $path\logs\Debug.log -Append
+	}
 
     #Check if there is more than "toKeep". If yes, then delete the oldest one.
 
     $NbLogs = (Get-ChildItem "$($log.log_path)\rotate\" -Filter "*$($log.log_name)" | Measure-Object).Count;
 
     if($NbLogs -gt $log.toKeep){
-
+		"The number of log to exceed the limit, deleting the oldest file." | Out-File -FilePath $path\logs\Debug.log -Append
         #removing the oldest log
         $FileDate = (Get-Date -Format g)
         $pathu = "$($log.log_path)\rotate\"
@@ -35,3 +39,5 @@ foreach($log in $log_list){
         Remove-Item $OldFile
     }
 }
+"All the operations are now finished." | Out-File -FilePath $path\logs\Debug.log -Append
+"" | Out-File -FilePath $path\logs\Debug.log -Append
